5e16c8ee46eef89712292c58f5ce7f544d9795a2
Logan Carlile <60319673+logancarlile97@users.noreply.github.com>
2021-05-08 00:08:45 -0400

    Update windowsUpsMonitor.py

--- windowsUpsMonitor.py
+++ windowsUpsMonitor.py
@@ -1,1 +1,49 @@
+import time
+import serial
+import subprocess
 
+serPort = '/dev/arduino/tty-1-1.4.1' #Related to the USB port the upsMonitor is plugged into
+online = False
+lastPwrOn = time.time()
+
+def pwrOn(pwrOnInterval):
+    global lastPwrOn
+    global online
+    if (time.time() - lastPwrOn > pwrOnInterval):
+        if(online == True):
+            lastPwrOn = time.time()
+            print("Power ON")
+            subprocess.run("cd /home/pi/HomelabShutdown && python3 ./mainPowerOn.py standalone", shell=True, text=True)
+            #Put pwrOn cmd here
+            
+            
+
+def upsStatus(serPort):
+    global online
+    ser = serial.Serial(serPort, 9600)
+    while(True):    
+        stat = ser.readline().decode('utf-8').rstrip()
+        ser.flush()
+        if(stat == "SHUTDOWN"):
+            online = False
+            print("SHUTDOWN!!!")
+            #time.sleep(30)
+            subprocess.run("cd /home/pi/HomelabShutdown && python3 ./mainShutdown.py noAuth", shell=True, text=True)
+            ##input("Press ENTER to continue")
+            break
+        elif(stat == "OFFLINE"):
+            online = False
+        elif(stat == "ONLINE"):
+            online = True
+            pwrOn(300)
+try:
+    upsStatus(serPort)
+except KeyboardInterrupt:
+    print("User Exit")
+except Exception as e:
+    print(f"ERROR!!!\n{e}")
+def findUPSMonitor():
+  try:
+    serPort = '/dev/arduino/tty-1-1.4.1' #Related to the USB port the upsMonitor is plugged into
+    online = False
+    lastPwrOn = time.time()
