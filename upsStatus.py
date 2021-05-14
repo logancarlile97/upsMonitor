8d7d4cf9296fbf70fedd05f45ab1e9309ed4cbae
Logan Carlile <logancarlile97@gmail.com>
2021-05-14 15:31:04 -0400

    Update upsStatus.py

--- upsStatus.py
+++ upsStatus.py
@@ -24,7 +24,8 @@ def upsStatus(serPort):
         
         global online
         ser = serial.Serial(serPort, 9600, timeout=None)
-        #ser.flush()
+        ser.reset_input_buffer()
+        #time.sleep(1)
         while(True):    
             stat = ser.readline().decode('utf-8').rstrip()
             #ser.flush()
